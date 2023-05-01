5f4b825058916c93c20d671e4371b294300f7935
tiagorahal <rahal.aires@gmail.com>
2023-04-30 23:09:55 -0300

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
+
+  // Enable CORS for all origins and headers
+  app.use(cors());
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
@@ -15,5 +19,4 @@ async function bootstrap() {
     console.log('[WEB]', config.get<string>('BASE_URL'));
   });
 }
-
 bootstrap();
